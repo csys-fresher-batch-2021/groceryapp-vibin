c1778aca2e8519bb6fd4bcaeebb6f4e75410fc87
Vibinkumar S <vibi2653@global.chainsys.com>
2021-06-02 15:28:20 +0530

    Add order history Test error cleared

--- src/test/java/in/vibin/service/AddOrderHistoryTest.java
+++ src/test/java/in/vibin/service/AddOrderHistoryTest.java
@@ -2,23 +2,27 @@ package in.vibin.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import in.vibin.model.OrderHistory;
+import in.vibin.model.OrderProduct;
 
 class AddOrderHistoryTest {
 
 	@Test
 	void testAddOrderHistory() {
-		OrderHistory orderHistory = new OrderHistory();
-		orderHistory.setMobileNumber(9659679989L);
+		OrderProduct orderHistory = new OrderProduct();
+		long mobileNumber=9659644868L;
 		orderHistory.setName("rice");
 		orderHistory.setPrice(35);
 		orderHistory.setOrderedQuantity(2);
 		orderHistory.setAmount(70);
-		SellingHistory.addOrderHistory(orderHistory);
+		List<OrderProduct>orderProductList=new ArrayList<>();
+		orderProductList.add(orderHistory);
+		SellingHistory.addOrderHistory(mobileNumber,orderProductList);
 		List<OrderHistory> orderHistoryList = SellingHistory.getOrderHistory();
 		int size = orderHistoryList.size();
 		assertTrue(size >= 1);
